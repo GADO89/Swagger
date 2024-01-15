e9af92d0c8e21b74da5cc9889b799a6ca94706a6
GADO89 <gadomahmoud377@gmail.com>
2024-01-15 09:40:54 +0200

    ApiOperation @Api@

--- src/main/java/com/spring/swagger/controller/EmployeeController.java
+++ src/main/java/com/spring/swagger/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.spring.swagger.controller;
 import com.spring.swagger.model.Employee;
 import com.spring.swagger.dao.EmployeeRepository;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,8 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //http://localhost:8080
-@Api(value = "This is The Employee Controller", description = "Controller Layer")
-@Tag(name = "Employee Controller Layer", description = "Controller Layer")
+@Api( description = "Controller Employee Layer")
 @RestController
 @RequestMapping("/api")
 //http://localhost:8080/api
@@ -22,41 +22,28 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
     //http://localhost:8080/api/save
    @PostMapping("/save")
-   @Operation(description = "save Employee", summary = "save Employee",
-   tags ="Employee Controller Layer",
-   responses = {
-           @ApiResponse(responseCode = "201", description = "Created")
-   })
+   @ApiOperation(value = "Save Employee",notes ="You muss pass object of Employee" )
    public ResponseEntity<Employee> save(@RequestBody Employee employee){
         return  ResponseEntity.status(HttpStatus.CREATED).body(
                 employeeRepository.save(employee));
 
     }
     @GetMapping("/show")
-    @Operation(description = "show Employee ",summary = "show Employee ",
-    tags = "Employee Controller Layer",
-    responses = {
-            @ApiResponse(responseCode = "200", description = "Success")
-    })
+    @ApiOperation(value = "show Employee",notes ="You muss pass id of Employee" )
     public ResponseEntity<Employee> show(@RequestParam Long id){
       return ResponseEntity.status(HttpStatus.OK).body(
 
                 employeeRepository.findById(id).get());
     }
     @DeleteMapping("/delete")
-    @Operation(description = "delete Employee", summary = "delete Employee",
-            tags ="Employee Controller Layer",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Success")
-            })    public void delete(@RequestParam Long id){
+    @ApiOperation(value = "delete Employee",notes ="You muss pass id of Employee" )
+    public void delete(@RequestParam Long id){
         employeeRepository.deleteById(id);
     }
+
     @PutMapping("/edit")
-    @Operation(description = "edit Employee", summary = "edit Employee",
-            tags ="Employee Controller Layer",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "OK")
-            })    public Employee edit(@RequestBody Employee employee){
+    @ApiOperation(value = "edit Employee",notes ="You muss pass object of Employee" )
+    public Employee edit(@RequestBody Employee employee){
         return   employeeRepository.save(employee);
     }
 }
--- src/main/java/com/spring/swagger/controller/PlayerController.java
+++ src/main/java/com/spring/swagger/controller/PlayerController.java
@@ -1,32 +1,26 @@
 package com.spring.swagger.controller;
 
-import com.spring.swagger.dao.EmployeeRepository;
-import com.spring.swagger.model.Employee;
+
 import io.swagger.annotations.Api;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.web.bind.annotation.*;
 
 //http://localhost:8080
-//@Api(value = "This is The Player Controller", description = "Controller Player Layer")
-@Tag(name = "Player Controller Layer", description = "Controller Player Layer")
+@Api( description = "Controller Player Layer")
 @RestController
 @RequestMapping("/api")
 //http://localhost:8080/api
 public class PlayerController {
 
     @GetMapping("/name")
-    @Tag(name = "Player Controller Layer")
+    @ApiOperation(value = "get Name",notes = "Empty Object",tags ="employee-controller" )
+    //@ApiOperation(value = "get Name",notes = "Empty Object",tags = "employee-controller\n")
     public String getName(){
         return "Mahmoud Khalil";
     }
 
     @GetMapping("/student")
-    @Tag(name = "Employee Controller Layer")
     public String geStudent(){
         return "I am Student (Mahmoud Khalil)";
     }
