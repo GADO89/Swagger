75802ace394b8a53d2d034912a47d14793f56eea
GADO89 <gadomahmoud377@gmail.com>
2024-01-15 05:33:22 +0200

    @Operation

--- src/main/java/com/spring/swagger/controller/EmployeeController.java
+++ src/main/java/com/spring/swagger/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package com.spring.swagger.controller;
 import com.spring.swagger.model.Employee;
 import com.spring.swagger.dao.EmployeeRepository;
 import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 //http://localhost:8080
-@Api(value = "This is The Employee Controller", description = "Controller layer")
-@Tag(name = "Employee Controller Layer")
+@Api(value = "This is The Employee Controller", description = "Controller Layer")
+@Tag(name = "Employee Controller Layer", description = "Controller Layer")
 @RestController
 @RequestMapping("/api")
 //http://localhost:8080/api
@@ -18,9 +22,15 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
     //http://localhost:8080/api/save
    @PostMapping("save")
-   @Tag(name = "Employee Controller Layer")
-   public void save(@RequestBody Employee employee){
-        employeeRepository.save(employee);
+   @Operation(description = "save Employee", summary = "save Employee",
+   tags ="Employee Controller Layer",
+   responses = {
+           @ApiResponse(responseCode = "201", description = "Created")
+   })
+   public ResponseEntity<Employee> save(@RequestBody Employee employee){
+        return  ResponseEntity.status(HttpStatus.CREATED).body(
+                employeeRepository.save(employee));
+
     }
     @GetMapping("/show")
     @Tag(name = "Employee Controller Layer")
